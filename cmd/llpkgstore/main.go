173ad7b4dbb7c90dea1722b305ba0f2149c4b910
PengPengPeng717 <872462877.com>
2025-08-21 09:39:43 +0800

    8_21_gen

--- cmd/llpkgstore/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// import cmd "github.com/goplus/llpkgstore/cmd/llpkgstore/internal"
-import cmd_python "github.com/goplus/llpkgstore/cmd/llpkgstore/internal_python"
-
-func main() {
-	// cmd.Execute()
-	cmd_python.Execute()
-}
